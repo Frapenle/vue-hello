eeef5c1b48c364e275a5fe9d926decf7e876a246
francescotrudu <francescotrudu84@gmail.com>
2022-12-12 16:36:11 +0100

    Insert img src v-bind

--- script/script.js
+++ script/script.js
@@ -4,7 +4,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Vue 3 is awesome'
+            message: 'Vue 3 is awesome',
+            imgOne: 'https://images.unsplash.com/photo-1506038634487-60a69ae4b7b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=766&q=80'
         }
     }
 }).mount('#app')
